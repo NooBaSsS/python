a9cf9cd076630147323b750fab1d9402587f282a
NooBaSsS <90272720+NooBaSsS@users.noreply.github.com>
2022-09-06 19:38:42 +0300

    Add files via upload

--- test1.py
+++ test1.py
@@ -1,5 +1,9 @@
 age = input("Enter you age")
-if int(age) >= 18:
+if int(age) >= 18 and int(age) <= 50:
     print ("входи")
-else:
+
+elif int(age) > 50:
+    print ("ты слишком стар для этого")
+
+elif int(age) < 18:
     print ("ухади!!")
