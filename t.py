08bc6bb1909b099febb4af208f6c07660ec02111
NooBaSsS <90272720+NooBaSsS@users.noreply.github.com>
2022-10-07 19:45:42 +0300

    Add files via upload

---
+++ t.py
@@ -0,0 +1,14 @@
+import turtle
+turtle.color("gray")
+turtle.shape("turtle")
+side = 5
+turtle.speed(0)
+for i in range(4):
+    turtle.forward(100)
+    turtle.right(90)
+turtle.left(60)
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+
+turtle.done()
