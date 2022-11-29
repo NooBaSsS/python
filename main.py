995ff5307fb3708f1706fb75661a948e07c44a47
NooBaSsS <90272720+NooBaSsS@users.noreply.github.com>
2022-11-29 19:39:02 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from random import randint, choice
 
 first_names = ("Жран", "Дрын", "Брысь", "Жлыг")
@@ -78,9 +79,8 @@ def show_hero(hero:list) -> None:
 def levelup(hero: list) -> None:
     """
     TODO: что растет с уровнем?
-    TODO: не прекращать повышать уровень, пока текущий опыт больше нужного для повышения
     """
-    if hero[4] >= hero[5]:
+    while hero[4] >= hero[5]:
         hero[3] += 1
         hero[5] = hero[3] * 100
         print(f"{hero[0]} получил {hero[3]} уровень\n")
@@ -88,18 +88,21 @@ def levelup(hero: list) -> None:
 
 def buy_item(hero: list, price: int, item: str) -> None:
     """
-    Покупает предмет за price монет и кладет его в инвентарь героя
+    Покупает предмет item за price монет и кладет его в инвентарь героя
     """
     if hero[9] >= price:
         hero[9] -= price
         hero[10].append(item)
         print(f"{hero[0]} купил {item} за {price} монет!")
     else:
-        print(f"У {hero[0]} нет столько монет! Не хватило {price - hero[9]} ")
+        print(f"У {hero[0]} нет столько монет! Не хватило {price - hero[9]}")
 
 
 def consume_item(hero: list, idx: str) -> None:
-    if idx <= len(hero[10]) - 1:
+    """
+    Удаляет предмет из инвентаря по индексу и дает герою эффект этого предмета
+    """
+    if idx <= len(hero[10]) - 1 and idx > -1:
         print(f"{hero[0]} употребил {hero[10][idx]}")
         if hero[10][idx] == "зелье":
             hero[1] += 10
@@ -108,35 +111,106 @@ def consume_item(hero: list, idx: str) -> None:
         elif hero[10][idx] == "яблоко":
             pass
         else:
-            print("ничего")        
+            print("Никакого эффекта")
         hero[10].pop(idx)
-    else: 
-        print("нет")
+    else:
+        print("Нет такого индекса!")
+    print("")
+
 
 def play_dice(hero: list, bet: int) -> None:
+    """
+    Ставка от 1 монеты до количества монет героя
+    Игрок и казино бросаю кости, кто больше, то забирает ставку
+    TODO: Как удача влияет на кости?
+    """
     if bet > 0:
         if hero[9] >= bet:
-            hero_score = randint(2, 12) 
+            hero_score = randint(2, 12)
             casino_score = randint(2, 12)
             print(f"{hero[0]} выбросил {hero_score}")
             print(f"Трактирщик выбросил {casino_score}")
             if hero_score > casino_score:
+                hero[9] += bet
                 print(f"{hero[0]} выиграл {bet} монет")
             elif hero_score < casino_score:
+                hero[9] -= bet
                 print(f"{hero[0]} проиграл {bet} монет")
             else:
-                print("ничья")
+                print("Ничья!")
         else:
-            print(f"нету")
-    else: 
-        print("ставка невозможна")
-def fight(hero:list) -> None:
+            print(f"У {hero[0]} нет денег на такую ставку!")
+    else:
+        print("Ставки начинаются от 1 монеты!")
+    print("")
+
+
+def combat_turn(attacker, defender):
+    if attacker[1] > 0:
+        damage = attacker[6]
+        defender[1] -= damage
+        print(f"{attacker[0]} ударил {defender[0]} на {damage} жизней!")
+
+
+def start_fight(hero: list) -> None:
+    """
+    Сделать врага
+    Зависит ли враг от уровня героя
+    Обмен ударами, пока у всех есть жизни
+    Формула аткаи и защиты?
+    После боя забрать опыт, деньги и предметы
+    Проверить левелап
+    Можно ли выпить зелье в бою?
+
+    Персонаж - это список
+    [0] name - имя
+    [1] hp_now - здоровье текущее
+    [2] hp_max - здоровье максимальное
+    [3] lvl - уровень
+    [4] xp_now - опыт текущий
+    [5] xp_next - опыт до следующего уровня
+    [6] attack - сила атаки, применяется в бою
+    [7] defence - защита, применяется в бою
+    [8] luck - удача
+    [9] money - деньги
+    [10] inventory - список предметов
+    """
+    enemy = make_hero(hp_now=30, xp_now=12, money=10, inventory=["меч"])
+    while hero[1] > 0 and enemy[1] > 0:
+        os.system("cls")
+        combat_turn(hero, enemy)
+        combat_turn(enemy, hero)
+        print("")
+        show_hero(hero)
+        show_hero(enemy)
+        input("\nНажмите ENTER чтобы сделать следующий ход")
+    combat_result(hero, enemy)
+
+def combat_result(hero, enemy) -> None:
+    os.system("cls")
+    if hero[1] > 0 and enemy[1] <= 0:
+        print(f"{hero[0]} победил!")
+        print(enemy[4], "опыта")
+        hero[4] += enemy[4]
+        print(enemy[9], "монет")
+        hero[9] += enemy[9]
+        print("забрал")
+        for item in enemy[10]:
+            print(item, end=",")
+        hero[10] += enemy[10]
+        levelup(hero)
+        """
+        TODO: 
+        Победитель получает опыт соперника,
+        деньги соперника и предметы соперника.
+        Проверить левелап.
+        """
+    elif hero[1] <= 0 and enemy[1] > 0:
+        print(f"{enemy[0]} победил!")
+    else:
+        print(f"{hero[0]} и {enemy[0]} пали в бою:(")
+
+
+def get_award(winner, loser):
+    
     pass
-    """
-    сделать врага
-    зависит от уровня героя?
-    обмен ударами пока есть хп
-    после боя забрать оп, деньги, предметы
-    проверить левелап
-    зелье в бою?  
-    """
